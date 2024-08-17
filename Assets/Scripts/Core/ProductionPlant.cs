b80f56fdba95f526a77dc9c392167452ae1a2de9
Fabian Friedrich <friedrich.fabian@gmx.net>
2024-08-17 15:55:19 +0200

    Added Production Plant which fills waggons

---
+++ Assets/Scripts/Core/ProductionPlant.cs
@@ -0,0 +1,35 @@
+﻿// /**
+//  * This file is part of: GMTK-JAM-2024
+//  * Created: 17.08.2024
+//  * Copyright (C) 2024 Fabian Friedrich
+//  * Distributed under the terms of the MIT license (cf. LICENSE.md file)
+//  **/
+
+using System;
+using TMPro;
+using UnityEngine;
+
+namespace F4B1.Core
+{
+    public class ProductionPlant: MonoBehaviour
+    {
+        [SerializeField] private int stored = 20;
+        [SerializeField] private int capacity = 30;
+        [SerializeField] private string resourceId;
+        [SerializeField] private TextMeshProUGUI capacityText;
+
+        private void Start()
+        {
+            capacityText.text = $"{stored}/{capacity}";
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            var waggon = other.GetComponentInChildren<Waggon>();
+            if (!waggon) return;
+            
+            stored -= waggon.Fill(resourceId, stored);
+            capacityText.text = $"{stored}/{capacity}";
+        }
+    }
+}
--- Assets/Scripts/Core/Waggon.cs
+++ Assets/Scripts/Core/Waggon.cs
@@ -5,12 +5,22 @@
 //  * Distributed under the terms of the MIT license (cf. LICENSE.md file)
 //  **/
 
+using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace F4B1.Core
 {
     public class Waggon : MonoBehaviour
     {
+
+        [SerializeField] private TextMeshProUGUI storedText;
+        [SerializeField] private int capacity = 10;
+        [SerializeField] private string storedResourceId = "empty";
+        [SerializeField] private int stored = 0; 
+        
+        
         public void Move(float speed, Vector2 direction)
         {
             var pos = transform.position;
@@ -20,6 +30,40 @@ namespace F4B1.Core
                 pos.y = Mathf.RoundToInt(pos.y);
             transform.position = pos;
             transform.Translate(direction * (speed * Time.deltaTime ));
+        }
+
+        public int Fill(string resourceId, int maxAmount)
+        {
+            if (storedResourceId != "empty" && storedResourceId != resourceId) return 0;
+
+            var diff = Mathf.Min(maxAmount, capacity - stored);
+            stored += diff;
+            storedText.text = $"{stored}/{capacity}";
+            
+            if (storedResourceId != resourceId && diff != 0)
+            {
+                getWaggonById(storedResourceId)?.SetActive(false);
+                getWaggonById(resourceId)?.SetActive(true);
+            }
+            storedResourceId = resourceId;
+            
+            return diff;
+        }
+
+        private GameObject getWaggonById(string id)
+        {
+            for (var i = 0; i < transform.childCount; i++)
+            {
+                var child = transform.GetChild(i).gameObject;
+                if (child.name == id)
+                    return child;
+            }
+
+            return null;
+        }
+        
+        public void Empty()
+        {
         }
     }
 }
