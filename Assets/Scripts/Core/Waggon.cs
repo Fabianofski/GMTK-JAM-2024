f60c5f05ee6ecb4b2d844149910d16dfefbd7e72
Fabian Friedrich <friedrich.fabian@gmx.net>
2024-08-17 22:47:27 +0200

    Added Waggon offset

--- Assets/Scripts/Core/Waggon.cs
+++ Assets/Scripts/Core/Waggon.cs
@@ -15,19 +15,34 @@ namespace F4B1.Core
     public class Waggon : MonoBehaviour
     {
 
+        [SerializeField] private Vector2 offset;
+        
         [SerializeField] private TextMeshProUGUI storedText;
         [SerializeField] private int capacity = 10;
         [SerializeField] private string storedResourceId = "empty";
-        [SerializeField] private int stored = 0; 
+        [SerializeField] private int stored = 0;
+
+        private Animator animator;
+        private static readonly int Y = Animator.StringToHash("y");
+        private static readonly int X = Animator.StringToHash("x");
+
+        private Vector2 oldDirection = new Vector2(-1, -1);
         
         public string GetResourceId() => storedResourceId;
 
         private void Start()
         {
+            animator = GetComponent<Animator>();
             storedText.text = $"{stored}/{capacity}";
             UpdateStoredResource(storedResourceId);
         }
 
+        private void UpdateAnimator(Vector2 direction)
+        {
+            animator.SetFloat(X, direction.x);
+            animator.SetFloat(Y, direction.y);
+        }
+        
         public void Move(float speed, Vector2 direction)
         {
             var pos = transform.position;
@@ -37,6 +52,20 @@ namespace F4B1.Core
                 pos.y = Mathf.RoundToInt(pos.y);
             transform.position = pos;
             transform.Translate(direction * (speed * Time.deltaTime ));
+            
+            UpdateAnimator(direction);
+            ApplyOffset(direction);
+            oldDirection = direction;
+        }
+
+        private void ApplyOffset(Vector2 direction)
+        {
+            var pos = transform.position;
+            if (direction.x == 0 && oldDirection.x != 0)
+                pos.y += direction.y * offset.y;
+            if (direction.y == 0 && oldDirection.y != 0)
+                pos.x += direction.x * offset.x;
+            transform.position = pos;
         }
 
         public int Fill(string resourceId, int maxAmount)
