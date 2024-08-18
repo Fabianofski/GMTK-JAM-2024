6d504c38f3d7653e370f0c985e7cfdcf9b84f39f
Fabian Friedrich <friedrich.fabian@gmx.net>
2024-08-18 19:24:33 +0200

    Added Procedural Generation of Map

---
+++ Assets/Scripts/Core/ProceduralGenerator/TerrainGenerator.cs
@@ -0,0 +1,101 @@
+﻿// /**
+//  * This file is part of: GMTK-JAM-2024
+//  * Created: 18.08.2024
+//  * Copyright (C) 2024 Fabian Friedrich
+//  * Distributed under the terms of the MIT license (cf. LICENSE.md file)
+//  **/
+
+using UnityEngine;
+
+namespace F4B1.Core.ProceduralGenerator
+{
+    public class TerrainGenerator
+    {
+        public enum BiomeType
+        {
+            Forest = 1,
+            Desert = 2,
+            Clay = 3
+        }
+
+        public enum BlockType
+        {
+            Water = 0,
+            Ground = 1
+        }
+
+        public float[,] GenerateNoiseMap(int width, int height, float scale,
+            int octaves, Vector2 position)
+        {
+            var noiseMap = new float[width, height];
+            for (var y = 0; y < height; y++)
+            for (var x = 0; x < width; x++)
+            {
+                var sampleX = (float)x / width * scale;
+                var sampleY = (float)y / height * scale;
+                var noise = 0f;
+                var frequency = 0f;
+                for (var oct = 1; oct < octaves; oct *= 2)
+                {
+                    frequency += 1f / oct;
+                    noise += 1f / oct * Mathf.PerlinNoise(oct * (sampleX + position.x * scale),
+                        oct * (sampleY + position.y * scale));
+                }
+
+                noise /= frequency;
+                noiseMap[x, y] = noise;
+            }
+
+            return noiseMap;
+        }
+
+        public int[,] NormalizeTerrain(
+            float[,] terrainMap, Vector2 position
+        )
+        {
+            var width = terrainMap.GetLength(0);
+            var height = terrainMap.GetLength(1);
+            var map = new int[width, height];
+
+            for (var y = 0; y < height; y++)
+            for (var x = 0; x < width; x++)
+            {
+                map[x, y] = terrainMap[x, y] > 0.2f ? (int)BlockType.Ground : (int)BlockType.Water;
+            }
+
+            return map;
+        }
+
+        public int[,] NormalizeBiome(
+            float[,] biomeMap
+        )
+        {
+            var width = biomeMap.GetLength(0);
+            var height = biomeMap.GetLength(1);
+            var map = new int[width, height];
+
+            for (var y = 0; y < height; y++)
+            for (var x = 0; x < width; x++)
+                if (biomeMap[x, y] < 0.35f)
+                    map[x, y] = (int)BiomeType.Clay;
+                else if (biomeMap[x, y] < 0.53f)
+                    map[x, y] = (int)BiomeType.Forest;
+                else
+                    map[x, y] = (int)BiomeType.Desert;
+            return map;
+        }
+
+        public int[,] MergeTerrainWithBiome(
+            int[,] terrainMap, int[,] biomeMap
+        )
+        {
+            var width = terrainMap.GetLength(0);
+            var height = terrainMap.GetLength(1);
+            for (var y = 0; y < height; y++)
+            for (var x = 0; x < width; x++)
+                if (terrainMap[x, y] == 1)
+                    terrainMap[x, y] = biomeMap[x, y];
+            return terrainMap;
+        }
+    }
+}
---
+++ Assets/Scripts/Core/ProceduralGenerator/TilemapCreator.cs
@@ -0,0 +1,54 @@
+﻿// /**
+//  * This file is part of: GMTK-JAM-2024
+//  * Created: 18.08.2024
+//  * Copyright (C) 2024 Fabian Friedrich
+//  * Distributed under the terms of the MIT license (cf. LICENSE.md file)
+//  **/
+
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using Random = UnityEngine.Random;
+
+namespace F4B1.Core.ProceduralGenerator
+{
+    public class TilemapCreator : MonoBehaviour
+    {
+        private int[,] map;
+        [SerializeField] private Tilemap tilemap;
+        [SerializeField] private TileBase[] tiles;
+        [SerializeField] private int size;
+        [SerializeField] private float scale;
+        [SerializeField] private int octaves;
+
+        private void Awake()
+        {
+            var terrainGenerator = new TerrainGenerator();
+            
+            var position = new Vector2(Random.Range(0, 100), Random.Range(0, 100));
+            var terrainNoise = terrainGenerator.GenerateNoiseMap(size, size, scale, octaves, position);
+            var terrainMap = map = terrainGenerator.NormalizeTerrain(terrainNoise, position);
+
+            position = new Vector2(Random.Range(0, 100), Random.Range(0, 100));
+            var biomeNoise = terrainGenerator.GenerateNoiseMap(size, size, scale, octaves, position);
+            var biomeMap = terrainGenerator.NormalizeBiome(biomeNoise);
+            
+            map = terrainGenerator.MergeTerrainWithBiome(terrainMap, biomeMap);
+
+            FillTilemap();
+        }
+
+        private void FillTilemap()
+        {
+            for (var x = 0; x < size; x++)
+            {
+                for (var y = 0; y < size; y++)
+                {
+                    var distance = Mathf.Sqrt(Mathf.Pow(x - size / 2, 2) + Mathf.Pow(y - size / 2, 2));
+                    var biome = distance > 8 ? map[x, y] : 1;
+                    var tile = tiles[biome];
+                    tilemap.SetTile(new Vector3Int(x - size / 2, y - size / 2, 0), tile);
+                }
+            }
+        }
+    }
+}
