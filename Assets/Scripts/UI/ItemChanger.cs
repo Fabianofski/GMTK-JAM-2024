b1d65f4779d420f1d0bb6957e856bf62ce60befe
Fabian Friedrich <friedrich.fabian@gmx.net>
2024-08-18 11:17:57 +0200

    Added toolbar

---
+++ Assets/Scripts/UI/ItemChanger.cs
@@ -0,0 +1,23 @@
+﻿// /**
+//  * This file is part of: GMTK-JAM-2024
+//  * Created: 18.08.2024
+//  * Copyright (C) 2024 Fabian Friedrich
+//  * Distributed under the terms of the MIT license (cf. LICENSE.md file)
+//  **/
+
+using UnityAtoms.BaseAtoms;
+using UnityEngine;
+
+namespace F4B1.UI
+{
+    public class ItemChanger : MonoBehaviour
+    {
+        [SerializeField] private StringVariable selectedItem;
+        [SerializeField] private string item;
+        
+        public void SelectItem(bool selected)
+        {
+            selectedItem.SetValue(selected ? item : "none");
+        }
+    }
+}
